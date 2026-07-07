ba560ad64ebe2d99903ecd20093b372c488c5542
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2026-07-07 23:35:38 +0100

    Bump deps

--- flake.nix
+++ flake.nix
@@ -14,8 +14,8 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       pname = "WoofWare.Whippet";
-      dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
-      dotnet-runtime = pkgs.dotnetCorePackages.runtime_9_0;
+      dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0;
+      dotnet-runtime = pkgs.dotnetCorePackages.runtime_10_0;
       version = "0.1";
       dotnetTool = dllOverride: toolName: toolVersion: hash:
         pkgs.stdenvNoCC.mkDerivation rec {
@@ -64,7 +64,7 @@
         buildInputs = [dotnet-sdk];
         packages = [
           pkgs.alejandra
-          pkgs.nodePackages.markdown-link-check
+          pkgs.lychee
           pkgs.shellcheck
           pkgs.xmlstarlet
         ];
